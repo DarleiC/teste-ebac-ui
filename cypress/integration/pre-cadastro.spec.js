8c40147a6cdc7c987cb4138ad0c3dd89366f0a41
darleicastro <darleicastro87@hotmail.com>
2022-02-01 23:11:08 -0300

    alteracoes no teste automatizado atribuindo variaveis

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -8,13 +8,17 @@ describe('Funcionalidade Pre cadastro', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
     it('Deve completar o pre cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.userName()).type('@ebac.com')
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.userName(nomeFaker) //AQUI ESTA ATRIBUINDO O USERNAME COMO O NAME FAKER
+        let dominio = '@ebac.com'
+        cy.get('#reg_email').type(emailFaker).type(dominio)
         cy.get('#reg_password').type('Teste123@teste')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
